799974715be6fd482c6c5c4a811742ffd7e2581a
Alex Oshchepkov <aaoshepkov@gmail.com>
2023-10-01 13:18:56 +0300

    Ну почти

--- HW5/test_demoqa.py
+++ HW5/test_demoqa.py
@@ -1,25 +1,36 @@
+import os
+
 from selene import be, have
 from selene.support.shared import browser
 
 
 def test_demoqa():
-    # Открытие Google
     browser.open("https://demoqa.com/automation-practice-form")
 
-    # Заполнение формы имя и проверка
-    browser.element('[placeholder="First Name"]').should(be.blank).type('Alex')
-    #browser.element('[placeholder="First Name"]').should(have.text('Alex'))
-    # Заполнение формы фамилия и проверка
-    browser.element('[placeholder="Last Name"]').should(be.blank).type('Osh')
-    #browser.element('[placeholder="Last Name"]').should(have.text('Osh'))
-    # Проверка наличия текста на странице
-    browser.element('[placeholder="name@example.com"]').should(be.blank).type('aaoshepkov@gmail.com')
+    browser.element('#firstName').type('Alex')
+
+    browser.element('#lastName').type('Osh')
+
+    browser.element('#userEmail').type('aaoshepkov@gmail.com')
 
     browser.element('[for="gender-radio-1"]').click()
-    browser.element('[placeholder="Mobile Number"]').should(be.blank).type('9659944488')
-    #browser.element('[id="dateOfBirthInput"]').click().clear().type('31 Jan 1979')
-    browser.element('[class="subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3"]').type('Python, Selene, AQA')
-    test
+
+    browser.element('#userNumber').type('79659944488')
+
+    browser.element('#dateOfBirthInput').click()
+
+    browser.element('.react-datepicker__year-select').type('1979')
+
+    browser.element('.react-datepicker__month-select').type('January')
+
+    browser.element('[class="react-datepicker__day react-datepicker__day--031"]').click()
+
+    browser.element('#subjectsInput').type('computer science').press_enter()
+
+    browser.element('[for="hobbies-checkbox-1"]').click()
+
+    browser.element('[for="uploadPicture"]').send_keys(os.path.abspath('files/jenkins.png'))
+
 
 def test_google_search_negative():
     # Открытие Google
