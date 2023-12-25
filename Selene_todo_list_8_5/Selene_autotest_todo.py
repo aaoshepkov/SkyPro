f83adb15e88d22bc83c37264c9bb0a7a60e9b48f
Alex Oshchepkov <aaoshepkov@gmail.com>
2023-12-25 22:32:13 +0300

    SkyPro

--- Selene_todo_list_8_5/Selene_autotest_todo.py
+++ Selene_todo_list_8_5/Selene_autotest_todo.py
@@ -2,11 +2,10 @@ import selene
 import pytest
 from selene import browser
 from selene import be, have
+import time
 
 def test_todo_list():
     browser.open('https://sky-todo-list.herokuapp.com/')
-
-    browser.element('[type="text"]').type('Selene auto').press_enter()
-    browser.should(have.text('Selene auto'))
-
-test_todo_list()
+    browser.element('.input[type="text"]').type('Selene auto').press_enter()
+    time.sleep(3)
+    test
