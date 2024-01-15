a1783c36465d10c8434fa00051568fd0e9e172d4
Aleksandr Oshchepkov <125588671+aaoshepkov@users.noreply.github.com>
2024-01-15 20:07:53 +0300

    Add files via upload

---
+++ lesson_1_task_6.py
@@ -0,0 +1,54 @@
+def print1():
+    number1 = 1
+    return number1
+result1 = print1()
+print(result1)
+def print2():
+    number2 = 2
+    return number2
+result2 = print2()
+print(result2)
+def print3():
+    number3 = 3
+    return number3
+result3 = print3()
+print(result3)
+def print4():
+    number4 = 4
+    return number4
+result4 = print4()
+print(result4)
+def print5():
+    number5 = 5
+    return number5
+result5 = print5()
+print(result5)
+def print6():
+    number6 = 6
+    return number6
+result6 = print6()
+print(result6)
+def print7():
+    number7 = 7
+    return number7
+result7 = print7()
+print(result7)
+def print8():
+    number8 = 8
+    return number8
+result8 = print8()
+print(result8)
+def print9():
+    number9 = 9
+    return number9
+result9 = print9()
+print(result9)
+
+def print0():
+    number0 = 0
+    return number0
+result0 = print0()
+print(result0)
+
+print(result8,result8,result0,result0,result5,result5,result5,result3,result5,result3,result5, sep='')
+
