d05cbca5fc3b27f07f33477930baafe0280fe36e
oschepkov.aa <aaoshepkov@gmail.com>
2024-01-17 10:34:04 +0300

    Исправил

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,1 +1,20 @@
+phone_number = ''
 
+def set_phone_number(num):
+    global phone_number
+    num = str(num)
+    phone_number = phone_number + num
+
+set_phone_number(8)
+set_phone_number(8)
+set_phone_number(0)
+set_phone_number(0)
+set_phone_number(5)
+set_phone_number(5)
+set_phone_number(5)
+set_phone_number(3)
+set_phone_number(5)
+set_phone_number(3)
+set_phone_number(5)
+
+print(phone_number)
