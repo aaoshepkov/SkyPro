5cea55665d3a8eef52c99467d6a434b5032d75bb
oschepkov.aa <aaoshepkov@gmail.com>
2024-01-18 16:38:43 +0300

    Решения задач 3 урока.

---
+++ OOP/Address.py
@@ -0,0 +1,8 @@
+class Address:
+
+    def __init__(self, index, city, street, building, appartment):
+        self.index = index
+        self.city = city
+        self.street = street
+        self.building = building
+        self.appartment = appartment
